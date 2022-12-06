1d618371f5b615bbf049dbd311ca6a6802835f03
hamza sghaier <sghhamza10@gmail.com>
2022-12-06 05:23:00 +0100

    Add categories api config

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,18 @@
+
+const devHost = "http://5.135.194.236" ;
+const devPort = "8181" ;
+const devUrl= devHost+":"+devPort+"/app/v1/api/" ;
+const jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NjQ0MDkwMjYsImlzcyI6ImVzaG9wIiwiZXhwIjoxNjY0NDEwODI2fQ.R9yCge_10pnDkSjL9aospCdvnRBGYFI4dsOsVaW7fkE"
+
+
+
+
+
+export const categoriesUrl = devUrl+"get_categories" ;
+export const TOKEN = 'Bearer '+jwt ;
+
+
+
 export const deviceSize = {
   xs: 425,
   sm: 768,
