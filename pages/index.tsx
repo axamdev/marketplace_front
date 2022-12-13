74ef615c11c226d3a95902d583bc5374c233691c
hamza sghaier <sghhamza10@gmail.com>
2022-12-13 04:10:47 +0100

    Fix Home navigation route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,201 @@
+import { Box, Container, Stack, styled } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import ShopLayout1 from "components/layouts/ShopLayout1";
+import MobileNavigationBar2 from "components/mobile-navigation/MobileNavigationBar2";
+import PageFooter from "components/page-footer/PageFooter";
+import SideNavbar from "components/page-sidenav/SideNavbar";
+import ProductFilterCard from "components/products/ProductFilterCard";
+import SEO from "components/SEO";
 import Setting from "components/Setting";
 import { NextPage } from "next";
-import { useState } from "react";
-import { Box, Button, Container, IconButton,styled } from "@mui/material";
-import { H1, Paragraph, Span } from "components/Typography";
-import DoneIcon from "@mui/icons-material/Done";
-import { Link as Scroll } from "react-scroll";
-import { FlexBox } from "components/flex-box";
-import LazyImage from "components/LazyImage";
-import Link from "next/link";
+import FurnitureShopAllProducts from "pages-sections/furnitureshop/FurnitureShopAllProducts";
+import FurnitureShopSection1 from "pages-sections/furnitureshop/FurnitureShopSection1";
+import FurnitureShopSection2 from "pages-sections/furnitureshop/FurnitureShopSection2";
+import TopProductsSection from "pages-sections/furnitureshop/TopProductsSection";
+import TopSellingProducts from "pages-sections/furnitureshop/TopSellingProducts";
+import { useEffect, useRef, useState } from "react";
+import api from "utils/api/furniture-shop";
+import api2 from "utils/api/gift-shop";
+import apiSlidersHome from "utils/api/axam-homesliders";
+import apiCategories, { DataCategories } from "utils/api/axam-category";
+import apiSections, { SectionsResponse } from "utils/api/axam-sections";
+import apiOffers, { OffersResponse } from "utils/api/axam-offers";
 
-const IndexPage: NextPage = () => {
-  const [filterDemo, setFilterDemo] = useState("");
+import { layoutConstant } from "utils/constants";
+import GiftShopSection1 from "pages-sections/giftshop/GiftShopSection1";
+import GiftShopServices from "pages-sections/giftshop/GiftShopServices";
+import GiftShopSection3 from "pages-sections/giftshop/GiftShopSection3";
+import TopCategorySection from "pages-sections/giftshop/TopCategorySection";
+import GiftShopPopularItems from "pages-sections/giftshop/GiftShopPopularItems";
+import GiftShopTopSales from "pages-sections/giftshop/GiftShopTopSales";
+import GiftShopAllProducts from "pages-sections/giftshop/GiftShopAllProducts";
+import apiSubCategories from "utils/api/axam-category";
+import Section1 from "pages-sections/fashion-shop-3/Section1";
 
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  gap: "1.75rem",
+  display: "flex",
+  padding: "0 !important",
+  ".sidenav": {
+    top: 0,
+    bottom: 0,
+    position: "relative",
+    transition: "all 350ms ease-in-out",
+    width: layoutConstant.grocerySidenavWidth,
+    minWidth: layoutConstant.grocerySidenavWidth,
+    // height: `calc(100vh - ${layoutConstant.headerHeight}px)`,
+    "& .MuiPaper-root": { borderRadius: 0 },
+    [theme.breakpoints.down("md")]: { display: "none" },
+  },
+  ".pageContent": {
+    // left: "unset",
+    position: "center",
+    width: `100%`,
+    [theme.breakpoints.down("md")]: { width: "100%", marginLeft: 0 },
+  },
+}));
+
+// ======================================================================
+type FurnitureShopProps = {
+  categoriesList?:DataCategories;
+  subcategoriesList:any[];
+  slidershomeList:any[];
+  sectionsList: SectionsResponse;
+  offersList: OffersResponse;
+  topNewProducts: any[];
+  furnitureProducts: any[];
+  topSellingProducts: any[];
+  furnitureShopNavList: any[]; 
+  giftShopNavList: any[];
+  popularProducts: any[];
+  giftShopProducts: any[];
+  topSailedProducts: any[];
+  giftShopServicesList: any[];
+  giftShopTopCategories: any[];
+};
+
+// ======================================================================
+
+const IndexPage: NextPage<FurnitureShopProps> = (props) => {
+  const pageContentRef = useRef<HTMLElement>();
+  const [sidebarHeight, setSidebarHeight] = useState(0);
+  const theme = useTheme();
+
+  useEffect(() => setSidebarHeight(pageContentRef.current.offsetHeight), []);
+
+
+
+  console.log("Sections list"+props.sectionsList.data)
   return (
-    <Box id="top" overflow="hidden" bgcolor="background.paper">
-           <Container id="section-1" sx={{ mt: 12, position: "relative" }}>
-        <Box maxWidth="830px" mx="auto" mb={6.5} textAlign="center">
-          <H1 fontSize="40px" mb={3} fontWeight="900">
-            <Span>Bienvenu chez AXAM</Span>
-          </H1>
+    <ShopLayout1 showTopbar={false} categoriesList={props.categoriesList} >
 
-          <Paragraph
-            fontSize="18px"
-            fontWeight={500}
-            maxWidth="540px"
-            mx="auto"
-            mb={5}
-          >
-       
-          </Paragraph>
+      <SEO title="AXAM" />
 
+      {/* HERO SECTION */}
+      {/* <FurnitureShopSection1 /> */}
+      {/* <GiftShopSection1 slidershomeList={props.slidershomeList}/> */}
+      <Section1 slidershomeList={props.slidershomeList} offersList={props.offersList.data}/>
+      <Container>
+        <StyledContainer>
+          {/* LEFT SIDEBAR */}
+          {/* <Box className="sidenav">
+            <SideNavbar
+              lineStyle="dash"
+              sidebarStyle="style2"
+              navList={props.furnitureShopNavList}
+              sidebarHeight={sidebarHeight || "85vh"}
+            />
+             {/* <ProductFilterCard /> */}
+          {/*  </Box> */}
+
+          <Box className="pageContent" ref={pageContentRef}>
+            {/* OFFER BANNERS */}
+             {/* <FurnitureShopSection2 /> */}
+            {/* <GiftShopSection3 offersList={props.offersList.data}/> */}
+
+            <Box my={10} className="categories">
+            <TopCategorySection categoriesList={props.categoriesList} />
+
+            </Box>
+            <Box className="pageContent" ref={pageContentRef}>
+          {/* <GiftShopServices serviceData={props.giftShopServicesList} /> */}
     
-          <FlexBox justifyContent="center" mb={16}>
-
-<Scroll to="demos" duration={400} offset={-72 - 16} smooth={true}>
-  
-<Link href="/axam">
-{/* <a target="_blank" rel="noopener noreferrer"> */}
-  
- 
-      <Button
-    variant="contained"
-    color="primary"
-    size="large"
-    sx={{ m: "0.5rem" }}
->
-    Marketplace
-  </Button>
-{/* </a> */}
-</Link>
-
-</Scroll>
-</FlexBox>
-
-
         </Box>
+          </Box>
+        </StyledContainer>
 
+       <Stack spacing={6} my={6}>
+
+       {props.sectionsList.data.map((item) => {
+        return (
+
+          <GiftShopPopularItems dataSections={item} productsData={props.popularProducts} />
+
+        );
+      })}
+
+{/* 
+
+          <GiftShopTopSales  productsData={props.topSailedProducts} />
+          <GiftShopAllProducts productsData={props.giftShopProducts} />
+          <TopProductsSection productsData={props.topNewProducts} />
+          <TopSellingProducts productsData={props.topSellingProducts} />
+          <FurnitureShopAllProducts productsData={props.furnitureProducts} />
+       */}
+        </Stack>
       </Container>
-    </Box>
+
+
+      {/* REMOVE THIS SETTING COMPONENT */}
+      <Setting />
+
+      <MobileNavigationBar2>
+        <SideNavbar
+          navList={props.furnitureShopNavList}
+          lineStyle="dash"
+          sidebarStyle="style2"
+        />
+      </MobileNavigationBar2>
+    </ShopLayout1>
   );
 };
+
+export async function getStaticProps() {
+  const slidershomeList=await  apiSlidersHome.getAllSlidersHome();
+  const categoriesList = await apiCategories.getAllCategories();
+  //const subCategoriesList = await apiSubCategories.getSubCategories();
+  const sectionsList = await apiSections.getAllSections();
+  const offersList = await apiOffers.getAllOffers();
+  const topNewProducts = await api.getTopNewProducts();
+  const furnitureProducts = await api.getFurnitureProducts();
+  const topSellingProducts = await api.getTopSellingProducts();
+  const furnitureShopNavList = await api.getFurnitureShopNavList();
+  const popularProducts = await api2.getPopularProducts();
+  const giftShopProducts = await api2.getGiftShopProducts();
+  const giftShopNavList = await api2.getGiftShopNavigation();
+  const topSailedProducts = await api2.getTopSailedProducts();
+  const giftShopServicesList = await api2.getGiftShopServiceList();
+  const giftShopTopCategories = await api2.getGiftShopTopCategories();
+  return {
+    props: {
+      slidershomeList,
+      categoriesList,
+
+      sectionsList,
+      offersList,
+      topNewProducts,
+      furnitureProducts,
+      topSellingProducts,
+      furnitureShopNavList,
+      giftShopNavList,
+      popularProducts,
+      giftShopProducts,
+      topSailedProducts,
+      giftShopServicesList,
+      giftShopTopCategories
+    },
+  };
+}
 
 export default IndexPage;
